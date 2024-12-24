d1aee81d9eda414e13c476345de50fe03454c9ad
korsNaike <kirillgrinev2003@mail.ru>
2024-12-24 06:25:39 +0300

    Исправление валидации

--- src/main/kotlin/com/korsnaike/patternsspringstudent/controller/StudentController.kt
+++ src/main/kotlin/com/korsnaike/patternsspringstudent/controller/StudentController.kt
@@ -2,13 +2,17 @@ package com.korsnaike.patternsspringstudent.controller
 
 import com.korsnaike.patternsspringstudent.entity.Student
 import com.korsnaike.patternsspringstudent.service.StudentService
+import jakarta.validation.ConstraintViolationException
 import jakarta.validation.Valid
+import jakarta.validation.ValidationException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
+import org.springframework.transaction.TransactionException
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/students")
@@ -26,8 +30,19 @@ class StudentController(@Autowired private val studentService: StudentService) {
     fun createStudent(@RequestBody @Valid student: Student): Student = studentService.save(student)
 
     @PutMapping("/{id}")
-    fun updateStudent(@PathVariable id: Long, @RequestBody student: Student): Student =
-        studentService.update(student.copy(id = id))
+    fun updateStudent(@PathVariable id: Long, @RequestBody student: Student): Student {
+        try {
+            return studentService.update(student.copy(id = id))
+        } catch (ex: TransactionException) {
+            if (ex.rootCause != null && ex.rootCause is ValidationException) {
+                throw ResponseStatusException(HttpStatus.BAD_REQUEST, (ex.rootCause as ValidationException).localizedMessage)
+            }
+            throw ex
+        } catch (ex: ValidationException) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, ex.localizedMessage)
+        }
+    }
+
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
